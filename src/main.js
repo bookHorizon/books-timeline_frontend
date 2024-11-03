51d08bf65292fee695fd0ba61471e4b6f83a0843
Ray <startup@9043672@gmail.com>
2024-11-03 19:11:01 +0800

    chore:setting element UI

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import './assets/main.css'
+import './styles/base.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import './styles/element/index.scss'
+import ElementPlus from 'element-plus'
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +11,6 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+    .use(router)
+    .use(ElementPlus)
+    .mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import ElementPlus from 'unplugin-element-plus/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    ElementPlus({
+      useSource: true,
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "~/styles/vendorOverrides/element.scss" as *;`,
+      },
+    },
+  },
 })
