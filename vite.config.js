8d85c28a64388fe806b44fe1ce15eec9f342b1cd
Ray <startup@9043672@gmail.com>
2024-11-04 23:52:22 +0800

    chore:setting element UI

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 import ElementPlus from 'unplugin-element-plus/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,17 +15,23 @@ export default defineConfig({
     ElementPlus({
       useSource: true,
     }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@use "~/styles/vendorOverrides/element.scss" as *;`,
-      },
-    },
-  },
+  css:{
+    preprocessorOptions:{
+      scss:{
+        api: 'modern-compiler',
+      }
+    }
+  }
 })
