8c58cb2084c20069d54a96b1e8b3aaa4aae44f57
Ray <startup@9043672@gmail.com>
2024-11-06 21:52:16 +0800

    feat:create Logo

---
+++ src/components/Logo.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>BOOK HORIZON</h1>
+</template>
+<style lang="scss">
+h1{
+  font-size: 24px;
+  line-height: 24px;
+  font-weight: 700;
+  letter-spacing: calc(24px*0.05);
+}
+</style>
