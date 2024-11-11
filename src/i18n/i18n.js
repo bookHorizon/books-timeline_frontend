7037da00a316612d0f128d38797c69134c5aa654
Ray <startup@9043672@gmail.com>
2024-11-11 16:28:23 +0800

    chore:i18n config

---
+++ src/i18n/i18n.js
@@ -0,0 +1,11 @@
+import { createI18n } from 'vue-i18n'
+
+const i18n = createI18n({
+  legacy: false, 
+  locale: 'en-US',
+  fallbackLocale: 'en',
+  globalInjection:true,
+  message:{}
+})
+
+export default i18n
