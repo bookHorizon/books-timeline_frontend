233e944985c25c3da0f6d07e1f9ad04d5dde1d63
Ray <startup@9043672@gmail.com>
2024-11-11 16:28:02 +0800

    feat:use i18n function

--- src/main.js
+++ src/main.js
@@ -3,13 +3,19 @@ import './style/base.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import ElementPlus from 'element-plus'
-
+import i18n from './i18n/i18n'
 import App from './App.vue'
 import router from './router'
+
+window.addEventListener("error", (e) => {
+    console.log(e);
+    console.error(e.message);
+});
 
 const app = createApp(App)
 
 app.use(createPinia())
     .use(router)
     .use(ElementPlus)
+    .use(i18n)
     .mount('#app')
