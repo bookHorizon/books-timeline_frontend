47ee2bbf1dc9544bc2682b985a409fdd9d9e0a7e
Ray <startup@9043672@gmail.com>
2024-11-13 23:35:49 +0800

    style:create element UI common button components

---
+++ src/components/buttons/OutlineButton.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { defineProps } from 'vue';
+defineProps({
+  fontColor:{
+    type:String,
+    default:'#000000'
+  },
+  borderColor:{
+    type:String,
+    default:'#000000'
+  }
+})
+
+</script>
+
+<template>
+  <el-button class="button" plain>
+    <slot></slot>
+  </el-button>
+</template>
+<style lang="scss" scoped>
+@use '@/assets/style/vendorOverrides/element.scss' as *;
+
+.button {
+  @include baseButton;
+}
+</style>
