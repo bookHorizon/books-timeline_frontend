4bf6eca7f26e120c5210e9b485d9aa4cbced5fac
Ray <startup@9043672@gmail.com>
2024-11-13 23:32:09 +0800

    chore:setting i18n config

--- src/i18n/i18n.js
+++ src/i18n/i18n.js
@@ -1,11 +1,19 @@
 import { createI18n } from 'vue-i18n'
+import zh from '../i18n/language/zh-TW.json'
+import en from '../i18n/language/en-US.json'
 
 const i18n = createI18n({
   legacy: false, 
-  locale: 'en-US',
-  fallbackLocale: 'en',
+  locale: 'zh-TW',
+  fallbackLocale: 'zh-TW',
   globalInjection:true,
-  message:{}
+  messages:{
+    'zh-TW':zh,
+    'en-US':en,
+  },
+  missing(locale,key){
+    console.log(locale,key);
+  }
 })
 
 export default i18n
