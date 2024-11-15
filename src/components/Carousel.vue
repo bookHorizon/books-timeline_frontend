1b310e0cd459027555f73ef94ef112e171b8d5f9
Ray <startup@9043672@gmail.com>
2024-11-15 19:18:49 +0800

    feat(carousel):add new file test

---
+++ src/components/Carousel.vue
@@ -0,0 +1,24 @@
+<script setup>
+// If you are using PurgeCSS, make sure to whitelist the carousel CSS classes
+import 'vue3-carousel/dist/carousel.css'
+import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
+
+import Card from './Card.vue';
+</script>
+
+<template>
+  <carousel :items-to-show="1.5">
+    <slide v-for="slide in 10" :key="slide">
+      <Card></Card>
+    </slide>
+
+    <template #addons>
+      <navigation />
+      <pagination />
+    </template>
+  </carousel>
+</template>
+
+<style lang="scss">
+
+</style>
