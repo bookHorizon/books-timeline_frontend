be6087ba155b39f656af56556fd51e593a22ce1c
Ray <startup@9043672@gmail.com>
2024-11-20 17:50:07 +0800

    style:modify breakpoint name

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -24,14 +24,14 @@ import { footerLinks } from '@/config/links';
   background-color: #999999;
   white-space:nowrap;
 
-  @include breakpoint($pad){
+  @include breakpoint($tablet){
     display: flex;
     flex-direction: column;
     padding: 37px 45px 37px 32px;
     gap: 24px;
   }
 
-  @include breakpoint($pc){
+  @include breakpoint($desktop){
     flex-direction: row;
     align-items: center;
     gap:120px;
@@ -44,7 +44,7 @@ import { footerLinks } from '@/config/links';
   flex-direction: column;
   gap:8px;
 
-  @include breakpoint($pad){
+  @include breakpoint($tablet){
     flex-direction: row; 
     justify-content: space-between;
     align-items: center;
@@ -54,7 +54,7 @@ import { footerLinks } from '@/config/links';
 
 .logo{
   line-height: 24px;
-  @include breakpoint($pad){
+  @include breakpoint($tablet){
     text-align: left; 
     line-height: 36px;
   }
@@ -64,7 +64,7 @@ import { footerLinks } from '@/config/links';
   @include body-2-b;
   display: flex;
   justify-content: space-between;
-  @include breakpoint($pad){
+  @include breakpoint($tablet){
     @include body-1-b;
     gap: 64px;
   }
@@ -72,7 +72,7 @@ import { footerLinks } from '@/config/links';
 
 .copyRight{
   @include body-3;
-  @include breakpoint($pad){
+  @include breakpoint($tablet){
     flex-basis: 100%;
   }
 }
