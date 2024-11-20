954aa0ad3702ce1ee63d196b018e016968fb28c9
Ray <startup@9043672@gmail.com>
2024-11-20 17:39:42 +0800

    feat:add element i18n provider and mask position modify fixed

--- src/App.vue
+++ src/App.vue
@@ -3,32 +3,41 @@ import Header from './components/layout/Header.vue';
 import Main from './components/layout/Main.vue';
 import Footer from './components/layout/Footer.vue';
 import Aside from './components/layout/Aside.vue';
+
+import zhTw from 'element-plus/dist/locale/zh-tw.mjs'
+import en from 'element-plus/dist/locale/en.mjs'
+
 import { globalStore } from '@/stores/globalStore';
 import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
 
 const global = globalStore();
-const { isOpenMenu } = storeToRefs(global)
+const { isOpenMenu,elementPlusI18n } = storeToRefs(global)
 
+const locale = computed(()=>elementPlusI18n.value === 'zh-tw' ? zhTw:en)
 </script>
 
 <template>
-  <el-container class="container">
-    <el-container class="mainContent" direction="vertical">
-        <Header></Header>
-          <Main></Main>
-        <Footer></Footer>
+  <el-config-provider :locale="locale">
+    <el-container class="container">
+      <el-container class="mainContent" direction="vertical">
+          <Header></Header>
+            <Main></Main>
+          <Footer></Footer>
+      </el-container>
+      <div v-show="isOpenMenu" class="mask"></div>
+      <Aside v-show="isOpenMenu"></Aside>
     </el-container>
-    <div v-show="isOpenMenu" class="mask"></div>
-    <Aside v-show="isOpenMenu"></Aside>
-  </el-container>
+  </el-config-provider>
 </template>
 
 <style scoped>
 .mask{
   width: 100%;
   height: 100vh;
-  position: absolute;
+  position: fixed;
   z-index: 20;
   background-color: rgba(255,255,255,.5);
 }
+
 </style>
