8cb287f04a93971c2194cf771f229e39b45be6d5
Ray <startup@9043672@gmail.com>
2024-11-21 21:51:26 +0800

    feat(content):create home function card content

---
+++ src/config/homeFunctionCard.js
@@ -0,0 +1,28 @@
+const cardArticleI18n = [
+  {
+    title:'home.function.itemTitle1',
+    content:'home.function.itemContent1',
+    img:new URL(`@/assets/img/home/light/p6.png`, import.meta.url).href,
+    darkImg:new URL(`@/assets/img/home/dark/p6.png`, import.meta.url).href,
+  },
+  {
+    title:'home.function.itemTitle2',
+    content:'home.function.itemContent2',
+    img:new URL(`@/assets/img/home/light/p5.png`, import.meta.url).href,
+    darkImg:new URL(`@/assets/img/home/dark/p5.png`, import.meta.url).href,
+  },
+  {
+    title:'home.function.itemTitle3',
+    content:'home.function.itemContent3',
+    img:new URL(`@/assets/img/home/light/p8.png`, import.meta.url).href,
+    darkImg:new URL(`@/assets/img/home/dark/p8.png`, import.meta.url).href,
+  },
+  {
+    title:'home.function.itemTitle4',
+    content:'home.function.itemContent4',
+    img:new URL(`@/assets/img/home/light/p7.png`, import.meta.url).href,
+    darkImg:new URL(`@/assets/img/home/dark/p7.png`, import.meta.url).href,
+  }
+]
+
+export default cardArticleI18n;
