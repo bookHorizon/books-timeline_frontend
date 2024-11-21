9f9816c0353fdedfb0e491fbcd3f70cf22567240
Ray <startup@9043672@gmail.com>
2024-11-22 01:05:39 +0800

    fix:mark not exist import path

--- src/api/account.js
+++ src/api/account.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { BASE_URL,header } from '@/api/apiConfig.js';
 import { ref } from 'vue'
-import token from '@/utilMethod/jwtToken.js'
-import cookie from '@/utilMethod/sessionToken.js'
-import { userStore } from '@/store/user.js';
+// import token from '@/utilMethod/jwtToken.js'
+// import cookie from '@/utilMethod/sessionToken.js'
+// import { userStore } from '@/store/user.js';
 //instance
 const accountAPI = axios.create({
   baseURL: `${BASE_URL}/account`,
