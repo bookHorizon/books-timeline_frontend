355101357e12c40a009d102c08a93dff04f5b1a6
Ray <startup@9043672@gmail.com>
2024-12-09 15:14:55 +0800

    style(86b31w92x):fix menu mask background color

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,7 @@ const locale = computed(()=>elementPlusI18n.value === 'zh-tw' ? zhTw:en)
   height: 100vh;
   position: fixed;
   z-index: 20;
-  background-color: rgba(255,255,255,.5);
+  background-color: var(--menu-mask-backgroundColor);
 }
 
 </style>
