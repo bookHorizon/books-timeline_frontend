e7bd6bc3d3f2563f3a3120759608c4bfc50ddd6b
Ray <startup@9043672@gmail.com>
2025-03-29 14:26:01 +0800

    chore:install vite plugin vue-devtools

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,12 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
+import VueDevtools from 'vite-plugin-vue-devtools'
 import ElementPlus from 'unplugin-element-plus/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(),VueDevtools(),
     ElementPlus({
       useSource: true,
     }),
