91057f6c385ba9847d1578f42a616892e705e9cb
Ray <startup@9043672@gmail.com>
2025-04-05 19:36:18 +0800

    feature:composables

---
+++ src/composables/useResizeObserver.js
@@ -0,0 +1,12 @@
+import {ref} from 'vue';
+
+const widthView = ref(window.innerWidth)
+window.addEventListener('resize',()=>{
+  widthView.value = window.innerWidth;
+})
+
+const isPhoneWidth = computed(()=>widthView.value<767)
+const isTabletWidth = computed(()=>widthView.value>=767&&1440>widthView.value)
+const isDesktopWidth = computed(()=>1440<=widthView.value) 
+
+export {isPhoneWidth,isTabletWidth,isDesktopWidth}
